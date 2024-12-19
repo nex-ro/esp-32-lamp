d3bf9a09114ae3ca6aca316183be4881fef0ce2e
NEXRO <105696039+nex-ro@users.noreply.github.com>
2024-12-19 22:06:17 +0700

    configurasi web

---
+++ main.js
@@ -0,0 +1,16 @@
+function setKamar(){
+    ledKamar.style.background='red'
+    imgKamar.src= "./assets/led-on.png"
+}
+function setDapur(){
+    ledDapur.style.background='red'
+    imgDapur.src= "./assets/led-on.png"
+}
+function setDepan(){
+    ledDepan.style.background='red'
+    imgDepan.src= "./assets/led-on.png"
+}
+function setBelakang(){
+    ledBelakang.style.background='red'
+    imgBelakang.src= "./assets/led-on.png"
+}
